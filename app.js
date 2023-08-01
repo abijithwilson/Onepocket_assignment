affeded7cbbe8c02d23208b5aa87d16c49af6c82
abijith <abhijithwilson123@gmail.com>
2023-08-01 11:31:09 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}...`);
+});
